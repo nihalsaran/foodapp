05bb2bdbce788a89aa94975e88e8e02b07b6c4f1
Nihal saran <nihalsarandasd@gmail.com>
2023-07-30 21:55:46 +0530

    Changes to be committed: 	modified:   ios/trialapp.xcodeproj/project.pbxproj 	modified:   src/screens/Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,73 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { TextInput } from 'react-native-gesture-handler'
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+
+      <Text style={styles.title}>
+        Admin Login
+      </Text>
+      <TextInput style={styles.inputStyle} placeholder={'Enter Email Id'} />
+      <TextInput style={styles.inputStyle} placeholder={'Enter password'}/>
+      <TouchableOpacity style={styles.loginbtn}>
+        <Text style={styles.btnText}>
+         Login 
+        </Text>
+
+      </TouchableOpacity>
+
     </View>
   )
 }
 
 export default Login
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: '800',
+    color: '#000',
+    marginTop: 200,
+    
+    alignSelf: 'center',
+
+  },
+
+  inputStyle: {
+    paddingLeft: 20,
+    height: 50,
+    alignSelf: 'center',
+    marginTop: 30,
+    borderWidth: 0.5,
+    borderRadius: 10,
+    width: '90%'
+
+  },
+
+  loginbtn:{
+    backgroundColor:'orange',
+    width:'90%',
+    height: 50,
+    alignSelf:'center',
+    borderRadius: 10,
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems:'center',
+
+
+
+  },
+
+  btnText:{
+    fontSize: 18,
+    fontWeight: "600",
+    color: '#000',  
+  }
+
+
+})
